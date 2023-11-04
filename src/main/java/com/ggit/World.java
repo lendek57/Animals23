08c268307347a064e0c2fc91d398c02911b2db0f
Lena Dekster <lenadekster@gmail.com>
2023-11-04 06:46:22 +0100

    [LAB-04] Initial commit

---
+++ src/main/java/com/ggit/World.java
@@ -0,0 +1,13 @@
+package com.ggit;
+
+import java.util.Random;
+
+public class World {
+    private static Random random = new Random();
+
+    public static void main(String[] args) {
+        Animal animal = new Animal(new Vector2D(random.nextInt(100), random.nextInt(100)));
+        System.out.println(animal.getPosition());
+        for (MapDirection direction : MapDirection.values()) animal.move(direction);
+    }
+}
