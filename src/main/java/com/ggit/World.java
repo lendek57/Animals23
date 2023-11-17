3b5334659594ac3ec2b511658522e828c16547c4
Lena Dekster <lenadekster@gmail.com>
2023-11-17 22:46:39 +0100

    Read program arguments

--- src/main/java/com/ggit/World.java
+++ src/main/java/com/ggit/World.java
@@ -4,8 +4,9 @@ import java.util.Random;
 
 public class World {
 
-    private static final int daysNumber = 10;
+    private static final int defaultDaysNumber = 10;
     public static void main(String[] args) {
+        int daysNumber = args.length > 0 ? Integer.parseInt(args[0]) : defaultDaysNumber;
         for (int i = 0; i < daysNumber; i++) {
             Simulation.simulateDay();
         }
